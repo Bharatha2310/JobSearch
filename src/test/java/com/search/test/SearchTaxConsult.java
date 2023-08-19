a328c461528775593d512feb0e8cb6afd62db72f
Bharatha2310 <bharathapranay2310@gmail.com>
2023-08-19 17:00:10 +0530

    Update the code to handle two xpath

--- src/test/java/com/search/test/SearchTaxConsult.java
+++ src/test/java/com/search/test/SearchTaxConsult.java
@@ -5,16 +5,18 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import com.baseclass.BaseClass;
 
+
 public class SearchTaxConsult extends BaseClass {
 
 	@Test
-	public void searchTaxConsultIndia() throws InterruptedException {
+	public void searchTaxConsultIndia() throws InterruptedException{
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
 		WebDriverWait wait = new WebDriverWait(driver, 100);
@@ -41,7 +43,7 @@ public class SearchTaxConsult extends BaseClass {
 		// inserting the loops
 		for (int i = 1; i <= 10; i++) { // Outer Loop
 
-			for (int j = 5; j <= 20; j++) { // Inner Loop
+			for (int j = 10; j <= 20; j++) { // Inner Loop
 
 				Thread.sleep(5000);
 				WebElement article = driver
@@ -69,10 +71,22 @@ public class SearchTaxConsult extends BaseClass {
 					}
 					String jobtitle = driver.getTitle();
 					if (jobtitle.contains(JobProfile)) {
-						WebElement naukriApply = driver.findElement(By.id("apply-button"));
-						Thread.sleep(3000);
-							naukriApply.click();
-							Thread.sleep(5000);
+						
+						 String applyButtonXPath = "//*[@id=\"apply-button\"]";
+				            String alternateButtonXPath = "//*[@id=\"root\"]/main/div[2]/div[1]/section[1]/div[2]/div[2]/button[2]";
+
+				            // Try locating and clicking the Apply button using explicit wait
+				            if (clickButton(driver, applyButtonXPath)) {
+				                System.out.println("Apply button clicked successfully.");
+				            } else {
+				                // If Apply button click fails, try the alternate button
+				                if (clickButton(driver, alternateButtonXPath)) {
+				                    System.out.println("Alternate button clicked successfully.");
+				                } else {
+				                    System.out.println("Both button clicks failed.");
+				                }
+				            }
+				            Thread.sleep(3000);
 							System.out.println("Job Applied successfully");				
 						driver.switchTo().window(tabs.get(0));
 					} else {
@@ -84,4 +98,16 @@ public class SearchTaxConsult extends BaseClass {
 			Thread.sleep(5000);
 		}
 	}
+	
+	 public static boolean clickButton(WebDriver driver, String xpath) {
+	        WebDriverWait wait = new WebDriverWait(driver, 10);
+	        
+	        try {
+	            WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+	            button.click();
+	            return true; // Click succeeded
+	        } catch (Exception e) {
+	            return false; // Click failed
+	        }
+	    }
 }
